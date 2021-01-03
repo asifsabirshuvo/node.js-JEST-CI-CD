272f65c7d54d9bba10eee6dc139159728f7fc1c5
asif sabir shuvo <shuvo@sharetrip.net>
2021-01-03 13:25:49 +0600

    added pingpong route modified

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.get('/', async(req, res) => {
     return res.json(info.data);
 });
 
+//just a pingpong route
 app.get('/ping', async(req, res) => {
     return res.json({
         data: 'pong'
